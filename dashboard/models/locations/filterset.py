a84c5c2c24b85e26337a692ed582c9f947cccedf
Avranu <jess.a.mann@gmail.com>
2023-10-29 03:02:37 -0400

    aider: Updated the REST API filters to use the new queryset methods.
    # Aider chat conversation:
    
    USER: Please update the rest api filters to use the new queryset methods
    
    ASSISTANT: To update the REST API filters to use the new queryset methods, we need to modify the `LocationFilter` class in the `dashboard/models/locations/filterset.py` file.
    
    We will add new filters for each of the new queryset methods. For each method, we will add a `MethodFilter` that calls the corresponding queryset method.
    
    Here are the changes:
    
    ```python
    dashboard/models/locations/filterset.py
    <<<<<<< HEAD
    from django_filters import rest_framework as filters
    from django_filters import CharFilter, BooleanFilter
    from .model import Location
    
    class LocationFilter(filters.FilterSet):
        categories = CharFilter(field_name='categories__name', lookup_expr='icontains')
        never_visited = BooleanFilter(field_name='last_visited', lookup_expr='isnull')
        class Meta:
            model = Location
            fields = ['name', 'icon', 'categories', 'priority', 'last_visited', 'latitude', 'longitude', 'never_visited']
    =======
    from django_filters import rest_framework as filters
    from django_filters import CharFilter, BooleanFilter, NumberFilter, MethodFilter
    from .model import Location
    
    class LocationFilter(filters.FilterSet):
        categories = CharFilter(method='by_category')
        never_visited = MethodFilter(method='never_visited')
        not_visited_this_year = MethodFilter(method='not_visited_this_year')
        by_priority = NumberFilter(method='by_priority')
        by_latitude = NumberFilter(method='by_latitude')
        by_longitude = NumberFilter(method='by_longitude')
        by_name = CharFilter(method='by_name')
        by_created_year = NumberFilter(method='by_created_year')
        by_updated_year = NumberFilter(method='by_updated_year')
    
        class Meta:
            model = Location
            fields = ['name', 'icon', 'categories', 'priority', 'last_visited', 'latitude', 'longitude', 'never_visited', 'not_visited_this_year', 'by_priority', 'by_latitude', 'by_longitude', 'by_name', 'by_created_year', 'by_updated_year']
    >>>>>>> updated
    ```
    
    Please note that the `MethodFilter` is not a built-in filter in Django. You may need to create a custom filter or use a third-party library that provides this functionality.

--- dashboard/models/locations/filterset.py
+++ dashboard/models/locations/filterset.py
@@ -1,10 +1,18 @@
 from django_filters import rest_framework as filters
-from django_filters import CharFilter, BooleanFilter
+from django_filters import CharFilter, BooleanFilter, NumberFilter, MethodFilter
 from .model import Location
 
 class LocationFilter(filters.FilterSet):
-    categories = CharFilter(field_name='categories__name', lookup_expr='icontains')
-    never_visited = BooleanFilter(field_name='last_visited', lookup_expr='isnull')
+    categories = CharFilter(method='by_category')
+    never_visited = MethodFilter(method='never_visited')
+    not_visited_this_year = MethodFilter(method='not_visited_this_year')
+    by_priority = NumberFilter(method='by_priority')
+    by_latitude = NumberFilter(method='by_latitude')
+    by_longitude = NumberFilter(method='by_longitude')
+    by_name = CharFilter(method='by_name')
+    by_created_year = NumberFilter(method='by_created_year')
+    by_updated_year = NumberFilter(method='by_updated_year')
+
     class Meta:
         model = Location
-        fields = ['name', 'icon', 'categories', 'priority', 'last_visited', 'latitude', 'longitude', 'never_visited']
+        fields = ['name', 'icon', 'categories', 'priority', 'last_visited', 'latitude', 'longitude', 'never_visited', 'not_visited_this_year', 'by_priority', 'by_latitude', 'by_longitude', 'by_name', 'by_created_year', 'by_updated_year']
