959d2ee97bd09d26d5838d85e73dcf1a8d80be25
Avranu <jess.a.mann@gmail.com>
2023-10-29 03:29:05 -0400

    aider: Added pagination to the REST API for locations and comments.
    # Aider chat conversation:
    
    USER: Ensure there is pagination in the REST API for locations and comments.
    
    ASSISTANT: To add pagination to the REST API for locations and comments, we need to modify the `LocationViewSet` and `CommentViewSet` classes in the `dashboard/models/locations/viewset.py` and `dashboard/models/comments/viewset.py` files respectively.
    
    We will use Django Rest Framework's built-in pagination classes. For this example, we will use `PageNumberPagination` which provides a simple page number based pagination.
    
    Here are the changes we need to make:
    
    1. In `dashboard/models/locations/viewset.py`:
    
    ```python
    dashboard/models/locations/viewset.py
    <<<<<<< HEAD
    from rest_framework import viewsets, status
    from rest_framework.response import Response
    from .model import Location
    from .serializer import LocationSerializer
    from .filterset import LocationFilter
    =======
    from rest_framework import viewsets, status, pagination
    from rest_framework.response import Response
    from .model import Location
    from .serializer import LocationSerializer
    from .filterset import LocationFilter
    >>>>>>> updated
    
    class LocationViewSet(viewsets.ModelViewSet):
        filter_class = LocationFilter
        def get_queryset(self):
            queryset = Location.objects.filter(user=self.request.user)
            filter_backends = [filters.DjangoFilterBackend]
            filterset_class = LocationFilter
            return queryset
        serializer_class = LocationSerializer
    <<<<<<< HEAD
    =======
        pagination_class = pagination.PageNumberPagination
    >>>>>>> updated
    ```
    
    2. In `dashboard/models/comments/viewset.py`:
    
    ```python
    dashboard/models/comments/viewset.py
    <<<<<<< HEAD
    from rest_framework import viewsets
    from .model import Comment
    from .serializer import CommentSerializer
    =======
    from rest_framework import viewsets, pagination
    from .model import Comment
    from .serializer import CommentSerializer
    >>>>>>> updated
    
    class CommentViewSet(viewsets.ModelViewSet):
        queryset = Comment.objects.all()
        serializer_class = CommentSerializer
    <<<<<<< HEAD
    =======
        pagination_class = pagination.PageNumberPagination
    >>>>>>> updated
    ```
    
    These changes will add pagination to the `Location` and `Comment` viewsets. By default, `PageNumberPagination` will provide 100 objects per page. You can customize this by setting the `PAGE_SIZE` attribute on the pagination class.

--- dashboard/models/comments/viewset.py
+++ dashboard/models/comments/viewset.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, pagination
 from .model import Comment
 from .serializer import CommentSerializer
 
--- dashboard/models/locations/viewset.py
+++ dashboard/models/locations/viewset.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets, status
+from rest_framework import viewsets, status, pagination
 from rest_framework.response import Response
 from .model import Location
 from .serializer import LocationSerializer
