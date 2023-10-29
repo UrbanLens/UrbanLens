6a80a1ce2f12fbd3e8f321f728760d3d2836a334
Avranu <jess.a.mann@gmail.com>
2023-10-29 02:56:56 -0400

    aider: Added a filter to the `LocationFilter` class in `filterset.py` to allow filtering locations that have never been visited.
    # Aider chat conversation:
    
    USER: Please ensure that the rest api can be used to filter locations by locations that have never been visited
    
    ASSISTANT: To implement this feature, we need to modify the `LocationFilter` class in the `filterset.py` file to add a new filter that checks if the `last_visited` field is `None`. This will allow us to filter locations that have never been visited.
    
    Here are the steps:
    
    1. Modify the `filterset.py` file to add a new filter for locations that have never been visited.
    
    Here is the *edit block* for the change:
    
    ```python
    dashboard/models/locations/filterset.py
    <<<<<<< HEAD
    from django_filters import rest_framework as filters
    from django_filters import CharFilter
    from .model import Location
    
    class LocationFilter(filters.FilterSet):
        categories = CharFilter(field_name='categories__name', lookup_expr='icontains')
        class Meta:
            model = Location
            fields = ['name', 'icon', 'categories', 'priority', 'last_visited', 'latitude', 'longitude']
    =======
    from django_filters import rest_framework as filters
    from django_filters import CharFilter, BooleanFilter
    from .model import Location
    
    class LocationFilter(filters.FilterSet):
        categories = CharFilter(field_name='categories__name', lookup_expr='icontains')
        never_visited = BooleanFilter(field_name='last_visited', lookup_expr='isnull')
        class Meta:
            model = Location
            fields = ['name', 'icon', 'categories', 'priority', 'last_visited', 'latitude', 'longitude', 'never_visited']
    >>>>>>> updated
    ```
    
    In the updated code, we've added a `BooleanFilter` named `never_visited` that checks if the `last_visited` field is `None`. This will allow us to filter locations that have never been visited by passing `never_visited=True` in the API request.

--- dashboard/models/locations/filterset.py
+++ dashboard/models/locations/filterset.py
@@ -1,9 +1,10 @@
 from django_filters import rest_framework as filters
-from django_filters import CharFilter
+from django_filters import CharFilter, BooleanFilter
 from .model import Location
 
 class LocationFilter(filters.FilterSet):
     categories = CharFilter(field_name='categories__name', lookup_expr='icontains')
+    never_visited = BooleanFilter(field_name='last_visited', lookup_expr='isnull')
     class Meta:
         model = Location
-        fields = ['name', 'icon', 'categories', 'priority', 'last_visited', 'latitude', 'longitude']
+        fields = ['name', 'icon', 'categories', 'priority', 'last_visited', 'latitude', 'longitude', 'never_visited']
