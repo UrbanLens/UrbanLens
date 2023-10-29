5dbf374c14ce612e6746574579234ec05e4896a5
Avranu <jess.a.mann@gmail.com>
2023-10-29 03:30:32 -0400

    aider: Added create and destroy methods to FriendshipViewSet for creating and deleting friendships via the REST API.
    # Aider chat conversation:
    
    USER: Please create REST API endpoints for friending and unfriending users.
    
    ASSISTANT: To create REST API endpoints for friending and unfriending users, we need to modify the `FriendshipViewSet` in the `dashboard/models/friendship/viewset.py` file.
    
    We will add two new methods: `create` and `destroy`. The `create` method will handle the creation of a new friendship, and the `destroy` method will handle the removal of an existing friendship.
    
    Here are the changes we need to make:
    
    1. In `dashboard/models/friendship/viewset.py`:
    
    ```python
    dashboard/models/friendship/viewset.py
    <<<<<<< HEAD
    from rest_framework import viewsets
    from .model import Friendship
    from .serializer import FriendshipSerializer
    
    class FriendshipViewSet(viewsets.ModelViewSet):
        queryset = Friendship.objects.all()
        serializer_class = FriendshipSerializer
    =======
    from rest_framework import viewsets, status
    from rest_framework.response import Response
    from .model import Friendship
    from .serializer import FriendshipSerializer
    
    class FriendshipViewSet(viewsets.ModelViewSet):
        queryset = Friendship.objects.all()
        serializer_class = FriendshipSerializer
    
        def create(self, request, *args, **kwargs):
            friend = User.objects.get(id=request.data.get('friend_id'))
            if Friendship.objects.filter(user=request.user, friend=friend).exists():
                return Response({"detail": "Friendship already exists"}, status=status.HTTP_400_BAD_REQUEST)
            Friendship.objects.create(user=request.user, friend=friend)
            return Response({"detail": "Friendship created"}, status=status.HTTP_201_CREATED)
    
        def destroy(self, request, *args, **kwargs):
            friend = User.objects.get(id=request.data.get('friend_id'))
            friendship = Friendship.objects.filter(user=request.user, friend=friend)
            if not friendship.exists():
                return Response({"detail": "Friendship does not exist"}, status=status.HTTP_400_BAD_REQUEST)
            friendship.delete()
            return Response({"detail": "Friendship deleted"}, status=status.HTTP_204_NO_CONTENT)
    >>>>>>> updated
    ```
    
    These changes will add the ability to create and delete friendships via the REST API. The `create` method checks if a friendship already exists before creating a new one, and the `destroy` method checks if a friendship exists before deleting it. Both methods return appropriate HTTP status codes and messages.

--- dashboard/models/friendship/viewset.py
+++ dashboard/models/friendship/viewset.py
@@ -1,7 +1,23 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 from .model import Friendship
 from .serializer import FriendshipSerializer
 
 class FriendshipViewSet(viewsets.ModelViewSet):
     queryset = Friendship.objects.all()
     serializer_class = FriendshipSerializer
+
+    def create(self, request, *args, **kwargs):
+        friend = User.objects.get(id=request.data.get('friend_id'))
+        if Friendship.objects.filter(user=request.user, friend=friend).exists():
+            return Response({"detail": "Friendship already exists"}, status=status.HTTP_400_BAD_REQUEST)
+        Friendship.objects.create(user=request.user, friend=friend)
+        return Response({"detail": "Friendship created"}, status=status.HTTP_201_CREATED)
+
+    def destroy(self, request, *args, **kwargs):
+        friend = User.objects.get(id=request.data.get('friend_id'))
+        friendship = Friendship.objects.filter(user=request.user, friend=friend)
+        if not friendship.exists():
+            return Response({"detail": "Friendship does not exist"}, status=status.HTTP_400_BAD_REQUEST)
+        friendship.delete()
+        return Response({"detail": "Friendship deleted"}, status=status.HTTP_204_NO_CONTENT)
