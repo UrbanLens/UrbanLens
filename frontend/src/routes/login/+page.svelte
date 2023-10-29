a53f052f71a9cfb9ff3d66b4e8f6aee830fdf3e9
Avranu <jess.a.mann@gmail.com>
2023-10-29 03:53:38 -0400

    Refactor API call in login page to remove unnecessary query parameter.

--- frontend/src/routes/login/+page.svelte
+++ frontend/src/routes/login/+page.svelte
@@ -24,7 +24,7 @@
   };
 
   const getSavedPlaces = async (id_token) => {
-    const response = await fetch('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=1500&type=restaurant&keyword=cruise&key=YOUR_API_KEY', {
+    const response = await fetch('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=1500&key=YOUR_API_KEY', {
       headers: {
         'Authorization': `Bearer ${id_token}`
       }
