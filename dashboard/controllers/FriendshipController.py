5fb6a23b491bb0c271f9241945d486a3ce0f85bc
Jess Mann <jess.a.mann@gmail.com>
2023-12-24 12:15:35 +0000

    aider: The friendship controller functionality has been moved from `views.py` to `FriendshipController.py`.
    # Aider chat conversation:
    
    ASSISTANT: To move the friendship controller functionality from `views.py` to `FriendshipController.py`, we need to:
    
    1. Remove the friendship related functions from `views.py`.
    2. Add these functions to `FriendshipController.py`.
    
    Here are the *SEARCH/REPLACE* blocks:
    
    dashboard/views.py
    ```python
    <<<<<<< SEARCH
    @login_required
    def request_friend(request):
        if request.method == 'POST':
            friend_username = request.POST.get('friend_username')
            friend = User.objects.get(username=friend_username)
            Friendship.objects.create(user=request.user, friend=friend)
            return HttpResponse('Friend request sent.')
    
    @login_required
    def list_friends(request):
        friends = Friendship.objects.filter(user=request.user)
        return render(request, 'dashboard/view_friends.html', {'friends': friends})
    =======
    >>>>>>> REPLACE
    ```
    
    dashboard/controllers/FriendshipController.py
    ```python
    <<<<<<< SEARCH
    =======
    from django.shortcuts import render
    from django.contrib.auth.decorators import login_required
    from dashboard.models.friendship.model import Friendship
    from django.http import HttpResponse
    from django.contrib.auth.models import User
    
    @login_required
    def request_friend(request):
        if request.method == 'POST':
            friend_username = request.POST.get('friend_username')
            friend = User.objects.get(username=friend_username)
            Friendship.objects.create(user=request.user, friend=friend)
            return HttpResponse('Friend request sent.')
    
    @login_required
    def list_friends(request):
        friends = Friendship.objects.filter(user=request.user)
        return render(request, 'dashboard/view_friends.html', {'friends': friends})
    >>>>>>> REPLACE
    ```

--- dashboard/controllers/FriendshipController.py
+++ dashboard/controllers/FriendshipController.py
@@ -46,3 +46,21 @@ def request_friend(request):
 def list_friends(request):
     friends = Friendship.objects.filter(user=request.user)
     return render(request, 'dashboard/view_friends.html', {'friends': friends})
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from dashboard.models.friendship.model import Friendship
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+
+@login_required
+def request_friend(request):
+    if request.method == 'POST':
+        friend_username = request.POST.get('friend_username')
+        friend = User.objects.get(username=friend_username)
+        Friendship.objects.create(user=request.user, friend=friend)
+        return HttpResponse('Friend request sent.')
+
+@login_required
+def list_friends(request):
+    friends = Friendship.objects.filter(user=request.user)
+    return render(request, 'dashboard/view_friends.html', {'friends': friends})
