246261e4b122a8d811fabb5b14541b2470d6f915
Avranu <jess.a.mann@gmail.com>
2023-12-24 04:02:14 -0500

    copy over changes from local 1/2

---
+++ bin/research.py
@@ -0,0 +1,32 @@
+import requests
+from utils.settings import INSTAGRAM_ACCESS_TOKEN, INSTAGRAM_GRAPH_URL, GOOGLE_LENS_API_KEY, GOOGLE_LENS_URL
+
+def fetch_urbex_posts(hashtag):
+    """
+    Fetches the most popular Instagram posts with the given hashtag.
+    """
+    # This is a placeholder for the actual API call, which would need to use the Instagram API.
+    response = requests.get(f"{INSTAGRAM_GRAPH_URL}search?access_token={INSTAGRAM_ACCESS_TOKEN}&q={hashtag}")
+    posts = response.json()
+    return posts
+
+def identify_image_location(image_url):
+    """
+    Uses Google Lens or a similar service to identify the location of an image.
+    """
+    # This is a placeholder for the actual API call, which would need to use the Google Lens API or similar.
+    response = requests.post(GOOGLE_LENS_URL, json={'image_url': image_url}, headers={'Authorization': f'Bearer {GOOGLE_LENS_API_KEY}'})
+    location_data = response.json()
+    return location_data
+
+def main():
+    hashtag = 'urbex'
+    posts = fetch_urbex_posts(hashtag)
+    
+    for post in posts:
+        image_url = post['image_url']  # Placeholder for the actual image URL field
+        location = identify_image_location(image_url)
+        print(f"Image URL: {image_url}, Identified Location: {location}")
+
+if __name__ == '__main__':
+    main()
---
+++ bin/settings.py
@@ -0,0 +1,7 @@
+# Instagram API credentials
+INSTAGRAM_ACCESS_TOKEN = 'your-access-token'
+INSTAGRAM_GRAPH_URL = 'https://graph.instagram.com/'
+
+# Google Lens or similar service API credentials
+GOOGLE_LENS_API_KEY = 'your-api-key'
+GOOGLE_LENS_URL = 'https://lens.googleapis.com/'
