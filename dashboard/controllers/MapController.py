c9263911d2abf64203ccca1bf98b54d6886736f2
Jess Mann <jess.a.mann@gmail.com>
2023-12-24 11:24:56 +0000

    aider: Refactored MapController methods to handle HTMX requests and responses and imported Location and Category models.
    # Aider chat conversation:
    
    ASSISTANT: To implement the HTMX functionality in `MapController.py`, we need to modify the methods to handle HTMX requests and responses. We will also need to import the `Location` and `Category` models from `dashboard.models.locations.model` and `dashboard.models.categories.model` respectively.
    
    Here are the *SEARCH/REPLACE* blocks:
    
    dashboard/controllers/MapController.py
    ```python
    <<<<<<< SEARCH
    from django.shortcuts import render
    from django.http import HttpResponse
    from dashboard.models import Pin
    =======
    from django.shortcuts import render, redirect
    from django.http import HttpResponse, HttpResponseRedirect
    from dashboard.models.locations.model import Location
    from dashboard.models.categories.model import Category
    >>>>>>> REPLACE
    ```
    
    dashboard/controllers/MapController.py
    ```python
    <<<<<<< SEARCH
    def view_map(request):
        pins = Pin.objects.all()
        return render(request, 'dashboard/map.html', {'pins': pins})
    =======
    def view_map(request):
        locations = Location.objects.all()
        return render(request, 'dashboard/map.html', {'locations': locations})
    >>>>>>> REPLACE
    ```
    
    dashboard/controllers/MapController.py
    ```python
    <<<<<<< SEARCH
    def edit_pin(request, pin_id):
        pin = Pin.objects.get(id=pin_id)
        if request.method == 'POST':
            # Update the pin based on the form data
            # Redirect to the map view
        else:
            # Render the edit form
            return render(request, 'dashboard/edit_pin.html', {'pin': pin})
    =======
    def edit_pin(request, location_id):
        location = Location.objects.get(id=location_id)
        if request.method == 'POST':
            # Update the location based on the form data
            location.name = request.POST.get('name')
            location.description = request.POST.get('description')
            location.latitude = request.POST.get('latitude')
            location.longitude = request.POST.get('longitude')
            location.save()
            return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
        else:
            # Render the edit form
            return render(request, 'dashboard/edit_location.html', {'location': location})
    >>>>>>> REPLACE
    ```
    
    dashboard/controllers/MapController.py
    ```python
    <<<<<<< SEARCH
    def add_pin(request):
        if request.method == 'POST':
            # Create a new pin based on the form data
            # Redirect to the map view
        else:
            # Render the add form
            return render(request, 'dashboard/add_pin.html')
    =======
    def add_pin(request):
        if request.method == 'POST':
            # Create a new location based on the form data
            name = request.POST.get('name')
            description = request.POST.get('description')
            latitude = request.POST.get('latitude')
            longitude = request.POST.get('longitude')
            Location.objects.create(name=name, description=description, latitude=latitude, longitude=longitude)
            return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
        else:
            # Render the add form
            return render(request, 'dashboard/add_location.html')
    >>>>>>> REPLACE
    ```
    
    dashboard/controllers/MapController.py
    ```python
    <<<<<<< SEARCH
    def search_pins(request):
        query = request.GET.get('q')
        pins = Pin.objects.filter(name__icontains=query)
        return render(request, 'dashboard/map.html', {'pins': pins})
    =======
    def search_pins(request):
        query = request.GET.get('q')
        locations = Location.objects.filter(name__icontains=query)
        return render(request, 'dashboard/map.html', {'locations': locations})
    >>>>>>> REPLACE
    ```
    
    Please note that you will need to create the corresponding HTML templates (`dashboard/map.html`, `dashboard/edit_location.html`, `dashboard/add_location.html`) and add the necessary HTMX attributes for interactivity.

--- dashboard/controllers/MapController.py
+++ dashboard/controllers/MapController.py
@@ -1,29 +1,40 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from dashboard.models import Pin
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseRedirect
+from dashboard.models.locations.model import Location
+from dashboard.models.categories.model import Category
 
 def view_map(request):
-    pins = Pin.objects.all()
-    return render(request, 'dashboard/map.html', {'pins': pins})
+    locations = Location.objects.all()
+    return render(request, 'dashboard/map.html', {'locations': locations})
 
-def edit_pin(request, pin_id):
-    pin = Pin.objects.get(id=pin_id)
+def edit_pin(request, location_id):
+    location = Location.objects.get(id=location_id)
     if request.method == 'POST':
-        # Update the pin based on the form data
-        # Redirect to the map view
+        # Update the location based on the form data
+        location.name = request.POST.get('name')
+        location.description = request.POST.get('description')
+        location.latitude = request.POST.get('latitude')
+        location.longitude = request.POST.get('longitude')
+        location.save()
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
     else:
         # Render the edit form
-        return render(request, 'dashboard/edit_pin.html', {'pin': pin})
+        return render(request, 'dashboard/edit_location.html', {'location': location})
 
 def add_pin(request):
     if request.method == 'POST':
-        # Create a new pin based on the form data
-        # Redirect to the map view
+        # Create a new location based on the form data
+        name = request.POST.get('name')
+        description = request.POST.get('description')
+        latitude = request.POST.get('latitude')
+        longitude = request.POST.get('longitude')
+        Location.objects.create(name=name, description=description, latitude=latitude, longitude=longitude)
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
     else:
         # Render the add form
-        return render(request, 'dashboard/add_pin.html')
+        return render(request, 'dashboard/add_location.html')
 
 def search_pins(request):
     query = request.GET.get('q')
-    pins = Pin.objects.filter(name__icontains=query)
-    return render(request, 'dashboard/map.html', {'pins': pins})
+    locations = Location.objects.filter(name__icontains=query)
+    return render(request, 'dashboard/map.html', {'locations': locations})
