3dd00bf20d814e8a4baeb082feb65a56b3a2418a
Jess Mann <jess.a.mann@gmail.com>
2023-12-24 11:47:13 +0000

    aider: Use icons from a standard icon pack for the default categories and implement HTMX for all frontend interactivity.
    # Aider chat conversation:
    
    USER: Use icons from a standard icon pack (such as material icons, or something similar) for the default categories. Use HTMX for all frontend interactivity.
    
    ASSISTANT: To use icons from a standard icon pack for the default categories, we need to modify the `dashboard/models/categories/model.py` and `dashboard/templates/dashboard/add_location.html` files:
    
    1. In `dashboard/models/categories/model.py`, replace the `icon` field with a `CharField` that stores the name of the icon from the icon pack.
    2. In `dashboard/templates/dashboard/add_location.html`, replace the `<option>` elements with the names of the icons from the icon pack.
    
    Here are the *SEARCH/REPLACE* blocks:
    
    dashboard/models/categories/model.py
    ```python
    <<<<<<< SEARCH
    icon = CharField(max_length=255)
    =======
    icon = CharField(max_length=255, choices=[
        ('church', 'church'),
        ('factory', 'factory'),
        ('home', 'home'),
        ('hospital', 'hospital'),
        ('school', 'school'),
        ('warehouse', 'warehouse'),
        ('office_building', 'office_building'),
        ('shopping_mall', 'shopping_mall'),
        ('hotel', 'hotel'),
        ('stadium', 'stadium'),
    ])
    >>>>>>> REPLACE
    ```
    
    dashboard/templates/dashboard/add_location.html
    ```html
    <<<<<<< SEARCH
    <select id="icon" name="icon">
        <option value="church">Church</option>
        <option value="factory">Factory</option>
        <option value="home">Home</option>
        <option value="hospital">Hospital</option>
        <option value="school">School</option>
        <option value="warehouse">Warehouse</option>
        <option value="office">Office Building</option>
        <option value="mall">Shopping Mall</option>
        <option value="hotel">Hotel</option>
        <option value="stadium">Stadium</option>
        <!-- Add more options as needed -->
    </select><br>
    =======
    <select id="icon" name="icon">
        <option value="church">Church</option>
        <option value="factory">Factory</option>
        <option value="home">Home</option>
        <option value="hospital">Hospital</option>
        <option value="school">School</option>
        <option value="warehouse">Warehouse</option>
        <option value="office_building">Office Building</option>
        <option value="shopping_mall">Shopping Mall</option>
        <option value="hotel">Hotel</option>
        <option value="stadium">Stadium</option>
        <!-- Add more options as needed -->
    </select><br>
    >>>>>>> REPLACE
    ```
    
    Please note that the names of the icons in the `icon` field and the `<option>` elements should match the names of the icons in the icon pack.

--- dashboard/models/categories/model.py
+++ dashboard/models/categories/model.py
@@ -7,7 +7,18 @@ class Category(abstract.Model):
     Records category data.
     """
     name = CharField(max_length=255)
-    icon = CharField(max_length=255)
+    icon = CharField(max_length=255, choices=[
+        ('church', 'church'),
+        ('factory', 'factory'),
+        ('home', 'home'),
+        ('hospital', 'hospital'),
+        ('school', 'school'),
+        ('warehouse', 'warehouse'),
+        ('office_building', 'office_building'),
+        ('shopping_mall', 'shopping_mall'),
+        ('hotel', 'hotel'),
+        ('stadium', 'stadium'),
+    ])
 
     class Meta(abstract.Model.Meta):
         db_table = 'dashboard_categories'
