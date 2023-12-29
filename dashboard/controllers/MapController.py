e1fdc86ca3f3fff194ae257c8f27329c67468546
Avranu <jess.a.mann@gmail.com>
2023-12-29 16:18:23 -0500

    aider: Import the `json` module in MapController.py.
    # Aider chat conversation:
    
    USER: init_map appears to be erroring. NameError at /dashboard/map/init/
    name 'json' is not defined
    Request Method:	GET
    Request URL:	http://localhost:8000/dashboard/map/init/
    Django Version:	4.2.8
    Exception Type:	NameError
    Exception Value:
    name 'json' is not defined
    Exception Location:	/home/jess/personal/UrbanLens/UL/dashboard/controllers/MapController.py, line 140, in init_map
    Raised during:	dashboard.controllers.MapController.init_map
    
    ASSISTANT: The error is because the `json` module is not imported in the `MapController.py` file. We need to import it at the top of the file.
    
    Here is the *SEARCH/REPLACE* block:
    
    dashboard/controllers/MapController.py
    ```python
    <<<<<<< SEARCH
    from django.shortcuts import render
    from django.http import HttpResponse, HttpResponseRedirect
    from django.urls import reverse
    from django.contrib.auth.decorators import login_required
    =======
    import json
    from django.shortcuts import render
    from django.http import HttpResponse, HttpResponseRedirect
    from django.urls import reverse
    from django.contrib.auth.decorators import login_required
    >>>>>>> REPLACE
    ```
    
    This change will import the `json` module, which provides the `dumps` function used in the `init_map` function.

--- dashboard/controllers/MapController.py
+++ dashboard/controllers/MapController.py
@@ -23,6 +23,7 @@
 *        2023-12-24     By Jess Mann                                                                                   *
 *                                                                                                                      *
 *********************************************************************************************************************"""
+import json
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
