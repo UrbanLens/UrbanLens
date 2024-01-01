775ea033651ef0b3a303a8c87a6f6f27e6db293f
Avranu <jess.a.mann@gmail.com>
2024-01-01 13:24:23 -0500

    Update perform_update method to include status when updating a location.

--- dashboard/models/locations/viewset.py
+++ dashboard/models/locations/viewset.py
@@ -65,7 +65,8 @@ class LocationViewSet(viewsets.ModelViewSet):
             return Response(status=status.HTTP_403_FORBIDDEN)
         serializer = self.get_serializer(instance, data=request.data, partial=True)
         serializer.is_valid(raise_exception=True)
-        self.perform_update(serializer)
+        status = serializer.validated_data.get('status', LocationStatus.NOT_VISITED)
+        self.perform_update(serializer, status)
         logger.info(f"Location with id {instance.id} updated")
         return Response(serializer.data)
 
