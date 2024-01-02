ce275876af47efa21052c932e1044d604bf886e4
Avranu <jess.a.mann@gmail.com>
2024-01-02 10:36:30 -0500

    aider: The location page now loads Smithsonian images via htmx and includes a loading placeholder for the section until it is fully loaded.
    # Aider chat conversation:
    
    USER: Change the location page so it loads smithsonian images via htmx, and does not slow down the page load time. Implement a "loading" placeholder for the section until it is fully loaded.
    
    ASSISTANT: To make this change we need to modify `dashboard/controllers/location.py` and `dashboard/templates/dashboard/pages/location/index.html`:
    
    1. In `dashboard/controllers/location.py`, remove the Smithsonian API call from the `view` method and create a new method `get_smithsonian_images` that will be called via htmx.
    2. In `dashboard/templates/dashboard/pages/location/index.html`, replace the Smithsonian section with a div that will load the Smithsonian images via htmx.
    
    Here are the *SEARCH/REPLACE* blocks:
    
    dashboard/controllers/location.py
    ```python
    <<<<<<< SEARCH
            # Get historic images from the Smithsonian's API
            smithsonian_images = smithsonian_gateway.get_data(location.name)
    =======
            # Smithsonian images will be loaded via htmx
    >>>>>>> REPLACE
    ```
    
    dashboard/controllers/location.py
    ```python
    <<<<<<< SEARCH
            return render(request, 'dashboard/pages/location/index.html', {
                'location': location,
                'smithsonian': smithsonian_images,
                #'google_places': google_places_images,
                #'search_results': recent_search_results,
            })
    =======
            return render(request, 'dashboard/pages/location/index.html', {
                'location': location,
                #'google_places': google_places_images,
                #'search_results': recent_search_results,
            })
    
        def get_smithsonian_images(self, request, location_id, *args, **kwargs):
            """
            Returns the Smithsonian images for a location.
            """
            # Get the location
            try:
                location : Location = Location.objects.get(id=location_id)
            except Location.DoesNotExist:
                return HttpResponse("Location does not exist", status=404)
    
            # Instantiate the SmithsonianGateway with the API key
            smithsonian_gateway = SmithsonianGateway(settings.SMITHSONIAN_API_KEY)
    
            # Get historic images from the Smithsonian's API
            smithsonian_images = smithsonian_gateway.get_data(location.name)
    
            return render(request, 'dashboard/pages/location/smithsonian.html', {
                'smithsonian': smithsonian_images,
            })
    >>>>>>> REPLACE
    ```
    
    dashboard/templates/dashboard/pages/location/index.html
    ```python
    <<<<<<< SEARCH
        <div class="smithsonian">
            <!-- [
                {'title': "Columnea 'Campus Sunset'", 'url': None, 'thumbnail': None},
                {'title': 'Calophyllum inophyllum L.', 'url': 'https://ids.si.edu/ids/deliveryService/id/ark:/65665/m39851075d3599483397693eacf141ceea', 'thumbnail': 'https://ids.si.edu/ids/deliveryService/id/ark:/65665/m39851075d3599483397693eacf141ceea/90'}
            ] -->
            <h2>Smithsonian Images</h2>
            {% for image in images %}
                {% if image.url == None %}
                    {% continue %}
                {% endif %}
    
                <div class="smithsonian-image">
                    <a href="{{ image.url }}" target="_blank">{{ image.title }}</a>
                    <img src="{{ image.thumbnail }}" alt="{{ image.title }}" />
                </div>
            {% endfor %}
        </div>
    =======
        <div hx-get="{% url 'dashboard:location-smithsonian' location.id %}" hx-trigger="load" class="smithsonian">
            <h2>Smithsonian Images</h2>
            <div class="loading">Loading...</div>
        </div>
    >>>>>>> REPLACE
    ```
    
    Please note that you will need to create a new URL pattern for the `get_smithsonian_images` view and include it in your Django URL configuration. The URL name should be 'dashboard:location-smithsonian'.

--- dashboard/controllers/location.py
+++ dashboard/controllers/location.py
@@ -58,8 +58,7 @@ class LocationController(LoginRequiredMixin, GenericViewSet):
         smithsonian_gateway = SmithsonianGateway(settings.SMITHSONIAN_API_KEY)
         google_places_gateway = GooglePlacesGateway(settings.GOOGLE_PLACES_API_KEY)
 
-        # Get historic images from the Smithsonian's API
-        smithsonian_images = smithsonian_gateway.get_data(location.name)
+        # Smithsonian images will be loaded via htmx
         '''
         Sample smithsonian data: 
         [{'title': "Columnea 'Campus Sunset'", 'url': None, 'thumbnail': None}, {'title': 'Bicyclus campus', 'url': None, 'thumbnail': None}, {'title': 'Pacific studies', 'url': None, 'thumbnail': None}, {'title': 'Terry Adkins : sculpture and painting, East Campus Galleries, Valencia Community College', 'url': None, 'thumbnail': None}, {'title': 'Rethinking campus life new perspectives on the history of college students in the United States Christine A. Ogren, Marc A. VanOverbeke, editors', 'url': None, 'thumbnail': None}, {'title': 'Plestiodon fasciatus', 'url': None, 'thumbnail': None}, {'title': 'Osmorhiza occidentalis (Nutt.) Torr.', 'url': 'https://ids.si.edu/ids/deliveryService/id/ark:/65665/m34b0d78659a4b4a69aaa8ed3053d1945c', 'thumbnail': 'https://ids.si.edu/ids/deliveryService/id/ark:/65665/m34b0d78659a4b4a69aaa8ed3053d1945c/90'}, {'title': 'Heteropterys sp.', 'url': None, 'thumbnail': None}, {'title': 'Murraya exotica L.', 'url': 'https://ids.si.edu/ids/deliveryService/id/ark:/65665/m384104033839044758020f2f5203a0554', 'thumbnail': 'https://ids.si.edu/ids/deliveryService/id/ark:/65665/m384104033839044758020f2f5203a0554/90'}, {'title': 'Calophyllum inophyllum L.', 'url': 'https://ids.si.edu/ids/deliveryService/id/ark:/65665/m39851075d3599483397693eacf141ceea', 'thumbnail': 'https://ids.si.edu/ids/deliveryService/id/ark:/65665/m39851075d3599483397693eacf141ceea/90'}]
@@ -73,7 +72,26 @@ class LocationController(LoginRequiredMixin, GenericViewSet):
 
         return render(request, 'dashboard/pages/location/index.html', {
             'location': location,
-            'smithsonian': smithsonian_images,
             #'google_places': google_places_images,
             #'search_results': recent_search_results,
+        })
+
+    def get_smithsonian_images(self, request, location_id, *args, **kwargs):
+        """
+        Returns the Smithsonian images for a location.
+        """
+        # Get the location
+        try:
+            location : Location = Location.objects.get(id=location_id)
+        except Location.DoesNotExist:
+            return HttpResponse("Location does not exist", status=404)
+
+        # Instantiate the SmithsonianGateway with the API key
+        smithsonian_gateway = SmithsonianGateway(settings.SMITHSONIAN_API_KEY)
+
+        # Get historic images from the Smithsonian's API
+        smithsonian_images = smithsonian_gateway.get_data(location.name)
+
+        return render(request, 'dashboard/pages/location/smithsonian.html', {
+            'smithsonian': smithsonian_images,
         })
