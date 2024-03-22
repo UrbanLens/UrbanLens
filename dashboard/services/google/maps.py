7ae94282ead1599059252555c2d2c9fbb41100e5
Avranu <jess.a.mann@gmail.com>
2024-03-22 15:57:38 -0400

    profile_id -> profile

--- dashboard/services/google/maps.py
+++ dashboard/services/google/maps.py
@@ -286,7 +286,7 @@ class GoogleMapsGateway(Gateway):
         location, created = Location.objects.get_nearby_or_create(
             latitude=latitude,
             longitude=longitude,
-            profile_id=profile.pk,
+            profile=profile,
             defaults=kwargs
         )
 
