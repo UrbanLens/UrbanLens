185fb1af7a6f67661f6684f7e238e01c557be867
Jess Mann <jess@jmann.me>
2026-08-10 12:42:32 -0400

    cleanup compiled js

--- src/urbanlens/dashboard/frontend/static/dashboard/js/saved-filter-detail.js
+++
@@ -1,7 +0,0 @@
-import {
-  installGlobalIconPicker
-} from "./achievements-ee3vhq76.js";
-import"./achievements-2vd5xdaq.js";
-
-// src/urbanlens/dashboard/frontend/ts/entries/saved-filter-detail.ts
-installGlobalIconPicker();
